a7a0ed35fbfb07fd5421468a83779e1d55d33a2c
ygaurangbhatt19 <gaurangbhatt19@gmail.com>
2021-10-16 20:35:53 +0530

    Redis

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from redis import Redis
+
 
 app=Flask(__name__)
+redis=Redis(host="localhost",port=6379)
 
 @app.route("/")
 def flaskapp():
-    return "Flask"
+    # key log in redis
+    redis.incr("log")
+    return 'Flask log: %s' % redis.get("log")
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=3333,debug=True)
